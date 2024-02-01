c2f9452fdf024fbedd6349def33bd70b7b3dfe94
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2024-02-01 18:39:24 +0700

    _2966DivideArrayIntoArraysWithMaxDifference

---
+++ java/leet-code/src/main/java/org/example/_2966DivideArrayIntoArraysWithMaxDifference/Main.java
@@ -0,0 +1,10 @@
+package org.example._2966DivideArrayIntoArraysWithMaxDifference;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums =new int[]{1,3,4,8,7,9,3,5,1};
+        int k = 2;
+        int[][] result = Solution.divideArray(nums, k);
+        System.out.println(result);
+    }
+}
