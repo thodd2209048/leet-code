294589d3755b81a6f6b281a6bc4a18b9f4424df6
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2024-02-02 14:00:49 +0700

    _1291SequentialDigits

--- java/leet-code/src/main/java/org/example/Main.java
+++ java/leet-code/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String oldName = "1768. Merge Strings Alternately";
+        String oldName = "1291. Sequential Digits";
         System.out.println(Utils.formatPackageName(oldName));
     }
 }
---
+++ java/leet-code/src/main/java/org/example/_1291SequentialDigits/Main.java
@@ -0,0 +1,8 @@
+package org.example._1291SequentialDigits;
+
+//https://leetcode.com/problems/sequential-digits/description/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
