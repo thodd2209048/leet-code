73dc9fd2d3e62d1d7ec92c225993c7bfac702faa
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2024-02-05 04:21:45 +0700

    _228SummaryRanges

--- java/leet-code/src/main/java/org/example/Main.java
+++ java/leet-code/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String oldName = "1291. Sequential Digits";
+        String oldName = "228. Summary Ranges";
         System.out.println(Utils.formatPackageName(oldName));
     }
 }
---
+++ java/leet-code/src/main/java/org/example/_228SummaryRanges/Main.java
@@ -0,0 +1,8 @@
+package org.example._228SummaryRanges;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,2,3,4,6,8,9};
+        System.out.println(Solution.summaryRanges(nums));
+    }
+}
