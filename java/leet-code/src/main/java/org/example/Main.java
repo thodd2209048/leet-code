075b276e4199ae1e60630cfb3bbab1723baac223
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2024-02-10 20:14:35 +0700

    _647PalindromicSubstrings

--- java/leet-code/src/main/java/org/example/Main.java
+++ java/leet-code/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String unformattedName = "279. Perfect Squares";
+        String unformattedName = "647. Palindromic Substrings";
         System.out.println(Utils.formatPackageName(unformattedName));
     }
 }
